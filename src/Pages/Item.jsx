179229cba830c8ab1467087436c6da80a34c9bb2
Krish2366 <krish236630@gmail.com>
2025-08-18 21:05:03 +0530

    change

--- src/Pages/Item.jsx
+++ src/Pages/Item.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../Components/Navbar.jsx";
-import user from "../assets/user.svg";
+import user from "../assets/User.svg";
 import { useNavigate, useParams, Link } from "react-router-dom";
 
 function Item() {
